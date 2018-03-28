6eedb6576b16838ea0aea31f1e3f94f63f104b92
rbabcock <rachael.s.babcock@gmail.com>
2018-03-28 16:46:25 -0500

    added a second randomNoun const to hopefully get a 2nd random noun

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const madSentence = () => {
     //generates random inbetween words to add to the fun
     const descriptiveNouns = ["a crazy old lady", "a wild orcha", "the wicked witch of the west", "Darth Vader", "Han Solo", "that one guy from down the corner", "an undercover hobo", "a crew of penguins", "Mary Poppins", "Gandalf the White", "Aladin's flying carpet", "the Pirates of the Caribbean", "Jack Sparrow"];
     const randomNoun = descriptiveNouns[Math.floor(Math.random()*descriptiveNouns.length)];
+    const randomNoun2 = descriptiveNouns[Math.floor(Math.random()*descriptiveNouns.length)];
     const verbs = ["turned", "pooped", "birthed", "magicked", "melted", "trampled", "fetched", "wrestled", "macerated", "chewed", "whispered", "scratched", "sewed", "buried", "break-danced", "jumped" ];
     const randomVerb = verbs[Math.floor(Math.random()*verbs.length)];
      //collect inputs into constants? probably let(s)
@@ -14,7 +15,7 @@ const madSentence = () => {
     let verbInput = $('#verb').val();
     let adjectiveInput = $('#adjective').val();
     let nounInput = $('#noun').val();
-    let HTMLstring = `<h2 id="output">${nameInput} ${verbInput1} into ${randomNoun} in ${locationInput} and there ${verbInput} with ${randomNoun} who ${randomVerb} ${nameInput} into a ${adjectiveInput} ${nounInput}</h2>`;
+    let HTMLstring = `<h2 id="output">${nameInput} ${verbInput1} into ${randomNoun} in ${locationInput} and there ${verbInput} with ${randomNoun2} who ${randomVerb} ${nameInput} into a ${adjectiveInput} ${nounInput}</h2>`;
     $('#mad-output').html(HTMLstring);
 };
 
