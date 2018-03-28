245887b75a7c5d64130a268729485249516506ce
rbabcock <rachael.s.babcock@gmail.com>
2018-03-28 16:33:24 -0500

    added MATH.random features so that some of the sentence structure would change each time

--- app.js
+++ app.js
@@ -2,14 +2,19 @@
 
 
 const madSentence = () => {
-        //collect inputs into constants? probably let(s)
+    //generates random inbetween words to add to the fun
+    const descriptiveNouns = ["a crazy old lady", "a wild orcha", "the wicked witch of the west", "Darth Vader", "Han Solo", "that one guy from down the corner", "an undercover hobo", "a crew of penguins", "Mary Poppins", "Gandalf the White", "Aladin's flying carpet", "the Pirates of the Caribbean", "Jack Sparrow"];
+    const randomNoun = descriptiveNouns[Math.floor(Math.random()*descriptiveNouns.length)];
+    const verbs = ["turned", "pooped", "birthed", "magicked", "melted", "trampled", "fetched", "wrestled", "macerated", "chewed", "whispered", "scratched", "sewed", "buried", "break-danced", "jumped" ];
+    const randomVerb = verbs[Math.floor(Math.random()*verbs.length)];
+     //collect inputs into constants? probably let(s)
     let nameInput = $('#name').val();
     let verbInput1 =$('#verb1').val();
     let locationInput = $('#location').val();
     let verbInput = $('#verb').val();
     let adjectiveInput = $('#adjective').val();
     let nounInput = $('#noun').val();
-    let HTMLstring = `<h2 id="output">${nameInput} ${verbInput1} into ${locationInput} and there ${verbInput} ${adjectiveInput} ${nounInput}</h2>`;
+    let HTMLstring = `<h2 id="output">${nameInput} ${verbInput1} into ${randomNoun} in ${locationInput} and there ${verbInput} with ${randomNoun} who ${randomVerb} ${nameInput} into a ${adjectiveInput} ${nounInput}</h2>`;
     $('#mad-output').html(HTMLstring);
 };
 
