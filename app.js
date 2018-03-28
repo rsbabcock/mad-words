c6a4444ae9732b54d7255f567424c2ad2d4fc55c
rbabcock <rachael.s.babcock@gmail.com>
2018-03-28 12:25:55 -0500

    Started working on getting js to work, then decided to add bootstrap, jquery and popper to index.html to have more jQurey options for writing code

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log("my js is working");
+//collect inputs into constants? probably let(s)
+let nameInput = console.log(document.getElementById('name').nodeValue);
+
+//concatenate inputs together and output them to empty div in html when submit button is pressed
+
+/*     <form>
+            <input class="form-item" id="name" placeholder="Name">
+            <input class="form-item" id="location" placeholder="Location">
+            <input class="form-item" id="verb" placeholder="Verb">
+            <input class="form-item" id="adjective" placeholder="Adjective">
+            <input class="form-item" id="noun" placeholder="Noun">
+            <button class="form-item" id="submit" type="button">Get a MAD sentence</button>
+ */
+
+
