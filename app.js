115693237b9d6eb04fd6c6b41fb40e076b37da1a
rbabcock <rachael.s.babcock@gmail.com>
2018-03-28 13:25:19 -0500

    got the app.js working and functional

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
 //concatenate inputs together and output them to empty div in html when submit button is pressed
-const madSentence = function() {
+
+
+const madSentence = () => {
         //collect inputs into constants? probably let(s)
     let nameInput = $('#name').val();
     let locationInput = $('#location').val();
     let verbInput = $('#verb').val();
     let adjectiveInput = $('#adjective').val();
     let nounInput = $('#noun').val();
-    return console.log(`${nameInput} ran into a ${locationInput} and there ${verbInput} into a ${adjectiveInput}${nounInput}`);
+    let HTMLstring = `<h2 id="output">${nameInput} ran into ${locationInput} and there ${verbInput} a ${adjectiveInput}${nounInput}</h2>`;
+    $('#mad-output').html(HTMLstring);
 };
 
-$('#form-submit').click(madSentence);
 
+//     let HTMLstring = `<h2>${nameInput} ran into a ${locationInput} and there ${verbInput} into a ${adjectiveInput}${nounInput}<h2>`;
+//     $('#mad-output').html(HTMLstring);
+// };
+
+$('#form-submit').click(madSentence);
+$('form-submit').toggle();
+
+
+
+//empty div html
+/* <div id="mad-output">
+<h2 id="output"></h2>
+</div> */
 
 
